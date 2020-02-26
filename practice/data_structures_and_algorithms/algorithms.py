793fd3135cdcdff4498e26f5cf0a6ef09940324e
tadaguides <58115007+tadaguides@users.noreply.github.com>
2020-02-26 00:29:56 -0500

    a
    a

--- practice/data_structures_and_algorithms/algorithms.py
+++
@@ -1,6 +0,0 @@
-'''
-Algorithms
-Sorts: Quick Sort, Merge Sort, Insertion Sort, Bucket Sort
-Searches: Binary Search, Depth First Search, Breadth First Search
-Algorithms: Greedy Algorithms, Dynamic Programming, Divide and Conquer
-'''
